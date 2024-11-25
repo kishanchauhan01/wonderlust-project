7da2096c4d1d188aaf7a827172cfbe3cd96a5f8e
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2024-11-25 13:22:54 +0530

    add some middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import methodOverride from "method-override";
 import ejsMate from "ejs-mate";
+import fs from "fs";
 
 const app = express();
 
@@ -16,7 +17,36 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 //pre Middleware
-app.use(express.urlencoded({ extended: true }));//extract data from url
+
+// app.use((req, res, next) => {
+//   let {token} = req.query;
+//   if(token === 'giveaccess') {
+//     next()
+//   } else {
+//     res.status(400).send("Access denied")
+//   }
+// })
+
+
+app.use((req, res, next) => {
+  req.time = new Date(Date.now());
+  const logData = `
+  ==========================
+  Method: ${req.method}
+  Hostname: ${req.hostname}
+  Path: ${req.path}
+  IP: ${req.ip}
+  Time: ${req.time}
+  ==========================\n`;
+  fs.appendFile(path.join(__dirname, "LOG.txt"), logData, (err) => {
+    if (err) {
+      console.log("Failed to write LOG: ", err);
+    }
+  });
+
+  next();
+});
+app.use(express.urlencoded({ extended: true })); //extract data from url
 app.use(methodOverride("_method"));
 
 //routes import
@@ -29,4 +59,10 @@ app.use(rootRouter);
 //listing router
 app.use("/api/v1/listings", listingRouter);
 
+
+
+
+app.use((req, res) => {
+    res.status(404  ).send("404, Page not found !")
+})
 export { app };
